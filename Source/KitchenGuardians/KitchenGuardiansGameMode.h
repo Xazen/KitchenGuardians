63ce3247e31070236f38b0e961fa0414010a917a
freyaprogrammer <freyaprogrammer@gmail.com>
2015-05-01 13:32:50 +0200

    freyaprogrammer - first setup

---
+++ Source/KitchenGuardians/KitchenGuardiansGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "KitchenGuardiansGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KITCHENGUARDIANS_API AKitchenGuardiansGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
