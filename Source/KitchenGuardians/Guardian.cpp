4c9210c60e227222df9346289da2471ddae2faac
freyaprogrammer <freyaprogrammer@gmail.com>
2015-07-15 16:50:49 +0200

    see last commit

--- Source/KitchenGuardians/Guardian.cpp
+++ Source/KitchenGuardians/Guardian.cpp
@@ -10,20 +10,13 @@ AGuardian::AGuardian()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	hitpoints = 2;
-	projectilesMaximum = 20;
-	projectilesCurrent = projectilesMaximum;
 	isDead = false;
-	doFlash = false;
-	isReloading = false;
-	reloadTime = 2.0f;
-	reloadTimeCurrent = 0.0f;
 }
 
 // Called when the game starts or when spawned
 void AGuardian::BeginPlay()
 {
 	Super::BeginPlay();
-	projectilesCurrent = projectilesMaximum;
 }
 
 // Called every frame
@@ -60,54 +53,4 @@ void AGuardian::Heal()
 	}
 
 
-}
-
-
-void AGuardian::Shot(int32 ammoCost)
-{
-	ShotFeedback();
-	projectilesCurrent -= ammoCost;
-}
-
-bool AGuardian::canShoot()
-{
-	if (hitpoints == 0)
-		return false;
-	if (isReloading)
-		return false;
-	if (projectilesCurrent == 0)
-		return false;
-	return true;
-
-}
-
-void AGuardian::reloadAmmo(float deltaSeconds)
-{
-	if (isReloading)
-	{
-		reloadTimeCurrent += deltaSeconds;
-		projectilesCurrent = reloadTimeCurrent;
-		if (reloadTimeCurrent >= reloadTime)
-		{
-			isReloading = false;
-			projectilesMaximum = reloadTmpAmmoStore;
-			projectilesCurrent = projectilesMaximum;
-		}
-
-	}
-	else{
-		if (projectilesCurrent == 0)
-		{
-			initReload();
-		}
-	}
-}
-
-void AGuardian::initReload()
-{
-	ReloadFeedback();
-	isReloading = true;
-	reloadTmpAmmoStore = projectilesMaximum;
-	projectilesMaximum = reloadTime;
-	reloadTimeCurrent = 0.0f;
 }
--- Source/KitchenGuardians/Guardian.h
+++ Source/KitchenGuardians/Guardian.h
@@ -45,45 +45,16 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GuardianPropsBalancing")
 		int32 hitpoints;
 
-
-	// initial and maximum amount of projectiles of this Guardian
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AmmunitionBalancing")
-		float projectilesMaximum; //float because of possible "over time" or "swipe length" depletion
-
-	// current amount of projectiles of this Guardian
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ammunition")
-		float projectilesCurrent; //float because of possible "over time" or "swipe length" depletion
-
-	// whether this Guardian is currently in a reload process
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ammunition")
-		bool isReloading;
-
-	// time in seconds the reload process takes
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AmmunitionBalancing")
-		float reloadTime;
-
-	// time the Current Reload process has past
-	UPROPERTY(BlueprintReadWrite, Category = "Ammunition")
-		float reloadTimeCurrent;
-
-	// temporarily stores the maximum amount of projectiles - because the maximum projectiles are used to display reload progress
-	UPROPERTY(BlueprintReadWrite, Category = "Ammunition")
-		float reloadTmpAmmoStore;
-
-
-	// Flag to check whether the player failed to revive
+	// Flag to check whether the player is Dead
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Feedback")
 		bool isDead;
 	
-	// bool used to execute a red screen flash
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Feedback")
-		bool doFlash;
 
 	///
 	///	UFUNCTIONS
 	///
 
-	// Calculates new Hitpoints after Guardian is hit by enemy - if necessary will initialize Revive Mechanic
+	// Calculates new Hitpoints after Guardian is hit by enemy
 	UFUNCTION(BlueprintCallable, Category = "HitpointsFunctions")
 		void GotHit(); // i am not sure whether the enemyType matters
 	
@@ -91,43 +62,17 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "HitpointsFunctions")
 		void Heal(); // i am not sure whether the enemyType matters
 
-
-	// trigger event for shotFeedback
-	UFUNCTION(BlueprintImplementableEvent, Category = "FeedbackFunctions")
-		void ShotFeedback(); 
-
-	// trigger event for gotHitFeedback
+	// Trigger event for gotHitFeedback
 	UFUNCTION(BlueprintImplementableEvent, Category = "FeedbackFunctions")
 		void GotHitFeedback(); 
 
-	// trigger event for HealedFeedback
+	// Trigger event for HealedFeedback
 	UFUNCTION(BlueprintImplementableEvent, Category = "FeedbackFunctions")
 		void HealedFeedback();
 
-	// trigger event for diedFeedback
+	// Trigger event for diedFeedback
 	UFUNCTION(BlueprintImplementableEvent, Category = "FeedbackFunctions")
 		void DiedFeedback();
-
-	// trigger event for reloadFeedback
-	UFUNCTION(BlueprintImplementableEvent, Category = "FeedbackFunctions")
-		void ReloadFeedback();
-
-	// Reduces the Ammunition by one - may not be applicable to Rice Guardian
-	UFUNCTION(BlueprintCallable, Category = "AmmunitionFunctions")
-		void Shot(int32 ammoCost);
-
-		// Whether this guardian can shoot or not (depending on ammunition and hitpoints and active-status)
-	UFUNCTION(BlueprintCallable, Category = "AmmunitionFunctions")
-		bool canShoot();
-
-	// this function executes manual reload, auto reload when empty and handles the time the reload takes
-	UFUNCTION(BlueprintCallable, Category = "AmmunitionFunctions")
-		void reloadAmmo(float deltaSeconds);
-
-	// this function executes manual reload, auto reload when empty and handles the time the reload takes
-	UFUNCTION(BlueprintCallable, Category = "AmmunitionFunctions")
-		void initReload();
-
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
